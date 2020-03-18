9038e65940b82e923172051359266f30ba3eea15
neilkapadia7 <neilkapadia7@gmail.com>
2020-03-18 23:47:42 +0530

    Initial Commit

--- src/actions/projectAtions.js
+++ src/actions/projectAtions.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-export const createProject = (project) => async dispatch => {
+export const createProject = (project) => async (dispatch, {getFirebase, getFireStore}) => {
     try {
         
 
--- src/store.js
+++ src/store.js
@@ -3,14 +3,22 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
+import {  reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
+
 const initialState = {}
 
-const middleware  = [thunk];
+const middleware  = [thunk.withExtraArgument({getFirebase, getFirestore})];
 
 const store = createStore(
     rootReducer,
     initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
+    composeWithDevTools(
+        applyMiddleware(...middleware),
+        reduxFirestore(fbConfig),
+        reactReduxFirebase(fbConfig)
+    )
 )
 
 export default store;
